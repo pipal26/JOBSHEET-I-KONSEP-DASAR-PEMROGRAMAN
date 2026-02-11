84241d8277a80f1ff0231a987ae4e4331a0a1abb
pipal26 <novalgg87@gmail.com>
2026-02-11 08:46:05 +0700

    Perulangan

---
+++ ProgramDeretBilangan.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ProgramDeretBilangan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Input NIM: ");
+        String nim = input.nextLine();
+        
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+        
+        if (n < 10) {
+            n += 10;
+        }
+        
+        System.out.print("Output: ");
+        
+        
+        for (int i = 1; i <= n; i++) {
+            
+            if (i == 10 || i == 15) {
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.print("# ");
+            }
+            else if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+            else {
+                System.out.print("* ");
+            }
+        }
+        
+        System.out.println();
+        input.close();
+    }
+}
